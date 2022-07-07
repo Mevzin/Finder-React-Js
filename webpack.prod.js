74247195cb3d59f6bb451b4a5c76629118324fb6
Thiago Torres <thiagomev@gmail.com>
2022-07-07 01:56:02 -0300

    add manifest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,21 @@
 // const { DefinePlugin } = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const WebpackPluginPWAManifest = require('webpack-plugin-pwa-manifest');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = merge( common, {
-  mode: 'production',
+module.exports = merge(common, {
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js(x?)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
@@ -25,21 +25,24 @@ module.exports = merge( common, {
         test: /\.(scss|sass)$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
-          }, {
-            loader: 'css-loader',
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
             options: {
-              modules: true
-            }
-          }, {
-            loader: 'sass-loader'
-          }
-        ]
-      }, {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     // new HtmlWebpackPlugin({
@@ -47,7 +50,7 @@ module.exports = merge( common, {
     //   favicon: "./src/assets/favicon.png",
     // }),
     new MiniCssExtractPlugin({
-      filename: 'main-bundle-[hash].css'
+      filename: "main-bundle-[hash].css",
     }),
     new FaviconsWebpackPlugin({
       logo: "./src/assets/favicon.png",
@@ -59,15 +62,25 @@ module.exports = merge( common, {
       favicon: "./public/favicon.png",
       manifest: "./public/manifest.json",
     }),
-    new WebpackPluginPWAManifest({
-      name: 'Finder App',
-      shortName: 'Finder',
-      startURL: './offline',
-      generateIconOptions: {
-        baseIcon: './public/favicon.png',
-        sizes: [192, 384, 512],
-        genFavicons: true
-      }
-    })
-  ]
-})
+    // new WebpackPwaManifest({
+    //   name: "Finder Api",
+    //   short_name: "Finder",
+    //   crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+    //   icons: [
+    //     {
+    //       src: path.resolve("public/favicon.png"),
+    //       sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+    //     },
+    //     {
+    //       src: path.resolve("public/favicon.png"),
+    //       size: "1024x1024", // you can also use the specifications pattern
+    //     },
+    //     {
+    //       src: path.resolve("public/favicon.png"),
+    //       size: "1024x1024",
+    //       purpose: "maskable",
+    //     },
+    //   ],
+    // }),
+  ],
+});
