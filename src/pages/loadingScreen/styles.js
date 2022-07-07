f2c6035e6462ad218ad4f8383f3ac7f8f13d3ea0
Thiago Torres <thiagomev@gmail.com>
2022-07-07 11:02:12 -0300

    Change loading page

--- src/pages/loadingScreen/styles.js
+++ src/pages/loadingScreen/styles.js
@@ -7,6 +7,7 @@ export const ContainerLoading = styled.div`
   flex-direction: column;
   color: ${({ theme }) => theme.colors.white};
   height: 100vh;
+  width: 100%;
 `;
 
 export const ImageLogo = styled.img`
